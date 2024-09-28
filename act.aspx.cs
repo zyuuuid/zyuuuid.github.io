ea61440083f08fb51e826f1464165096977884ae
yuzhuolong1993 <yuzhuolong1993@gmail.com>
2024-09-28 13:10:42 -0700

    add raw files

---
+++ act.aspx.cs
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" ContentType="text/html" ResponseEncoding="utf-8" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题文档</title>
+</head>
+
+<body>
+
+public partial class AddNumber : System.Web.UI.Page
+{
+    private static int count = 1;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        count ;
+        Response.Write("var pv=document.getElementById(pv); pv.innerText=" count ";");
+    }
+}
+
+</body>
+</html>
